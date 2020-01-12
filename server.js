b8d38d7bb73413e1e0215578692d1646e5de2741
bpetryshyn <bpetryshyn@hubspot.com>
2020-01-12 11:59:04 +0200

    Setup dotenv and extract server PORT config to config.env file.

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const bootcamps = require('./routes/bootcamps');
 
+require('dotenv').config({path: './config/config.env'});
+
 const app = express();
 
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(8080, () => console.log('The server has started.'));
+app.listen(
+  process.env.PORT,
+  () => console.log('The server has started.')
+);
