7fa44d0784d799dd4af395b45dd99ea12b05de04
bpetryshyn <bpetryshyn@hubspot.com>
2020-01-12 14:21:51 +0200

    Extract bootstrap controller from routes.

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,38 +1,24 @@
 const express = require("express");
 
+const {
+  getBootcamp,
+  getBootcamps,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp
+} = require("../controllers/bootcamps");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, data: { message: "Getting all bootcamps" } });
-});
+router
+  .route("/")
+  .get(getBootcamps)
+  .post(createBootcamp);
 
-router.get("/:id", (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: { message: `Getting bootcamp with id=${req.params.id}` }
-  });
-});
-
-router.post("/", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, data: { message: "Creating a bootcamp" } });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: { message: `Updating bootcamp with id=${req.params.id}` }
-  });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: { message: `Deleting bootcamp with id=${req.params.id}` }
-  });
-});
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
