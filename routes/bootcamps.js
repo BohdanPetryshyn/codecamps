86440ffe6ab50bae2a6b5e4cf946a89ab74d0e94
bpetryshyn <bpetryshyn@hubspot.com>
2020-01-12 13:50:44 +0200

    Add prettier and prettify the code.

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,25 +1,38 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({success: true, data: {message: 'Getting all bootcamps'}});
+router.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, data: { message: "Getting all bootcamps" } });
 });
 
-router.get('/:id', (req, res) => {
-  res.status(200).json({success: true, data: {message: `Getting bootcamp with id=${req.params.id}`}});
+router.get("/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: { message: `Getting bootcamp with id=${req.params.id}` }
+  });
 });
 
-router.post('/', (req, res) => {
-  res.status(200).json({success: true, data: {message: 'Creating a bootcamp'}});
+router.post("/", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, data: { message: "Creating a bootcamp" } });
 });
 
-router.put('/:id', (req, res) => {
-  res.status(200).json({success: true, data: {message: `Updating bootcamp with id=${req.params.id}`}});
+router.put("/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: { message: `Updating bootcamp with id=${req.params.id}` }
+  });
 });
 
-router.delete('/:id', (req, res) => {
-  res.status(200).json({success: true, data: {message: `Deleting bootcamp with id=${req.params.id}`}});
+router.delete("/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: { message: `Deleting bootcamp with id=${req.params.id}` }
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-const express = require('express');
-const bootcamps = require('./routes/bootcamps');
+const express = require("express");
+const bootcamps = require("./routes/bootcamps");
 
-require('dotenv').config({path: './config/config.env'});
+require("dotenv").config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use('/api/v1/bootcamps', bootcamps);
+app.use("/api/v1/bootcamps", bootcamps);
 
-app.listen(
-  process.env.PORT,
-  () => console.log('The server has started.')
-);
+app.listen(process.env.PORT, () => console.log("The server has started."));
