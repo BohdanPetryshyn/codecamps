5e7610cdfa60332f719e563967efd0689a45c989
bpetryshyn <bpetryshyn@hubspot.com>
2020-01-12 16:01:30 +0200

    Connect to MongoDB using mongoose

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+module.exports = async () => {
+  const connection = await mongoose.connect(
+    process.env.MONGO_CONNECTION_STRING,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+
+  console.log(`MongoDB connected at host=${connection.connection.host}`);
+};
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bootcamps = require('./routes/bootcamps');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 require('dotenv').config({ path: './config/config.env' });
+
+connectDB();
 
 const app = express();
 
