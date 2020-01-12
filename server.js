9939a6a8108a24103bb1e8d2fc9b6bf1843e0ccf
bpetryshyn <bpetryshyn@hubspot.com>
2020-01-12 14:28:23 +0200

    Add request logging middleware.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bootcamps = require("./routes/bootcamps");
+const requestMiddleware = require('./middleware/requestLogger');
 
 require("dotenv").config({ path: "./config/config.env" });
 
 const app = express();
+
+// Setup middleware
+app.use(requestMiddleware);
 
 app.use("/api/v1/bootcamps", bootcamps);
 
