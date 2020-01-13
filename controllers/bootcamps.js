132b7c06d499b5619024c24bdef5c4be7c966a9b
bpetryshyn <bpetryshyn@hubspot.com>
2020-01-13 08:49:21 +0200

    Change the bootcamp create status to 201

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -15,7 +15,7 @@ exports.getBootcamp = (req, res, next) => {
 
 exports.createBootcamp = async (req, res, next) => {
   const savedBootcamp = await Bootcamp.create(req.body);
-  res.status(200).json({
+  res.status(201).json({
     success: true,
     data: savedBootcamp,
   });
