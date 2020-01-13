c0e9807588974c54c563098bcd160672174c2006
bpetryshyn <bpetryshyn@hubspot.com>
2020-01-13 09:10:16 +0200

    Add bootcamp update logic and unify other endpoints return body

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,13 +2,19 @@ const Bootcamp = require('../models/Bootcamp');
 
 exports.getBootcamps = async (req, res, next) => {
   const bootcamps = await Bootcamp.find();
-  res.status(200).json(bootcamps);
+  res.status(200).json({
+    success: true,
+    data: bootcamps,
+  });
 };
 
 exports.getBootcamp = async (req, res, next) => {
   const id = req.params.id;
   const bootcamp = await Bootcamp.findById(id);
-  res.status(200).json(bootcamp);
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
 };
 
 exports.createBootcamp = async (req, res, next) => {
@@ -19,10 +25,12 @@ exports.createBootcamp = async (req, res, next) => {
   });
 };
 
-exports.updateBootcamp = (req, res, next) => {
+exports.updateBootcamp = async (req, res, next) => {
+  const id = req.params.id;
+  const bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body);
   res.status(200).json({
     success: true,
-    data: { message: `Updating bootcamp with id=${req.params.id}` },
+    data: bootcamp,
   });
 };
 
