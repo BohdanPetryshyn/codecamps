574949298748d64e28459db8755c07edd402166f
bpetryshyn <bpetryshyn@hubspot.com>
2020-01-13 07:24:56 +0200

    Add Bootcamp mongoose schema.

---
+++ models/Bootcamp.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    maxlength: 50,
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: true,
+    maxlength: 500,
+  },
+  url: {
+    type: String,
+    match: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/,
+  },
+  phone: {
+    type: String,
+    maxlength: 20,
+  },
+  email: {
+    type: String,
+    match: /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(\".+\"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    type: [String],
+    enum: [
+      'Web Development',
+      'Mobile Development',
+      'UI/UX',
+      'Data Science',
+      'Business',
+      'Other',
+    ],
+    required: true,
+  },
+  averageRating: {
+    type: Number,
+    min: 0,
+    max: 10,
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg',
+  },
+  housing: {
+    type: Boolean,
+    default: false,
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false,
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false,
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
