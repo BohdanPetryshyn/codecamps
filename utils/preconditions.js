d41ddfb09e25d29d76eea8a083afeff398c1ba43
bpetryshyn <bpetryshyn@hubspot.com>
2020-01-25 22:04:11 +0200

    Add ensureNumber precondition.

--- utils/preconditions.js
+++ utils/preconditions.js
@@ -6,3 +6,9 @@ exports.ensurePresence = (value, message) => {
   if (isNil(value)) throw new ApiError(400, message);
   return value;
 };
+
+exports.ensureNumber = (value, message) => {
+  const parsed = Number(value);
+  if (isNaN(parsed)) throw new ApiError(400, message);
+  return parsed;
+}
