3bd2724fdbecc8f9e073205bd5940f83ba79e641
bpetryshyn <bpetryshyn@hubspot.com>
2020-01-27 10:03:20 +0200

    Populating courses with bootcamps to which they belong to

--- controllers/courses.js
+++ controllers/courses.js
@@ -2,7 +2,11 @@ const Course = require('../models/Course');
 const passErrors = require('../utils/passErrors');
 
 exports.getCourses = passErrors(async (req, res, next) => {
-  const courses = await Course.find();
+  const courses = await Course.find().populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
   res.status(200).json({
     success: true,
     count: courses.length,
@@ -13,7 +17,10 @@ exports.getCourses = passErrors(async (req, res, next) => {
 exports.getBootcampCourses = passErrors(async (req, res, next) => {
   const bootcampId = req.params.id;
 
-  const courses = await Course.find({ bootcamp: bootcampId });
+  const courses = await Course.find({ bootcamp: bootcampId }).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
 
   res.status(200).json({
     success: true,
